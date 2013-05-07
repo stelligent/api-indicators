0d7b3bab76fae52e2b2674e011af0988a8f60c25
Alexander <skiba-alex@ya.ru>
2013-05-07 22:10:52 +0700

    Add user controllers and views

---
+++ app/assets/javascripts/sessions.js.coffee
@@ -0,0 +1,2 @@
+$(document).ready ->
+  $("form input#name").focus()
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    if user = User.find_by_name(params[:name]) and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to profile_path
+    else
+      flash.now.alert = "Invalid name or password"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "Logged out"
+  end
+end

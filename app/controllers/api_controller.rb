da21b4fd0c333fc6ec54f47bcb2f331fff0950df
Alexander <skiba-alex@ya.ru>
2013-05-09 21:13:45 +0700

    JSON accept header

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,11 +1,18 @@
 class ApiController < ApplicationController
   skip_before_filter :verify_authenticity_token
+  before_filter :json_by_default
   before_filter :restrict_api_access, except: [ :index, :show ]
 
 private
   def restrict_api_access
     authenticate_or_request_with_http_token do |token, options|
       User.exists?(api_key: token)
+    end
+  end
+
+  def json_by_default
+    unless request.headers["HTTP_ACCEPT"]
+      request.format = "json"
     end
   end
 end

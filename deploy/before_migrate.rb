5e060c0e738262cd4bde34ebd074251ec930d9be
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2013-12-08 11:33:30 -1000

    hmmmmmmm

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,7 +1,7 @@
-rails_env = new_resource.environment["RAILS_ENV"]
-Chef::Log.info("Setting up database for RAILS_ENV=#{rails_env}...")
-execute "rake db:setup" do
-  cwd release_path
-  command "bundle exec rake db:setup"
-  environment "RAILS_ENV" => rails_env
-end
+# rails_env = new_resource.environment["RAILS_ENV"]
+# Chef::Log.info("Setting up database for RAILS_ENV=#{rails_env}...")
+# execute "rake db:setup" do
+#   cwd release_path
+#   command "bundle exec rake db:setup"
+#   environment "RAILS_ENV" => rails_env
+# end

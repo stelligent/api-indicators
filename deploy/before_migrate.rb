9e51b5c61cc742f87184968485bcc7e8003510e0
Eric Kascic <eric.kascic@stelligent.com>
2013-12-19 13:50:03 -0500

    echo not a resource and this isnt a shell script duh

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -1,3 +1,1 @@
-echo "cd #{node[:deploy]['canaryboard'][:deploy_to]} && bundle exec rake db:setup"
-echo "UYOOOOOO!!!!"
 run "cd #{node[:deploy]['canaryboard'][:deploy_to]} && bundle exec rake db:setup"

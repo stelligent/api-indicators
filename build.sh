e4e4cc3ac4921f2257acfeb7a9dc0e1b7a68804b
Jonny Sywulak <jonathan.sywulak@stelligent.com>
2014-01-24 15:44:49 -0600

    adding build file

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+# check the syntax of each ruby file
+echo Syntax check...
+find . -name *.rb | xargs -n1 ruby -c > /dev/null
+
+# Prep and run unit tests
+gem install bundler
+bundle install
+bundle exec rake db:setup
+bundle exec rake db:test:prepare
+bundle exec rspec spec/ -f d
+
+# Run static analysis
+gem install brakeman --version 2.1.1
+brakeman -o brakeman-output.tabs
